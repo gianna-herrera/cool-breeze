38eca3fc29a11215247a4bd3c48696b3e11d27be
gianna-herrera <giannaherreracarrion@gmail.com>
2026-07-31 20:05:19 -0500

    Add about, art, cocktails, contact and menu sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import gsap from "gsap";
-import { ScrollTrigger, SplitText} from "gsap/all";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero"
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <main>
-        <Navbar></Navbar>
-        <Hero></Hero>
-
-        <div className="h-dvh bg-black"></div>
-    </main>
-  )
+ return (
+	<main>
+	 <Navbar />
+	 <Hero />
+	 <Cocktails />
+	 <About />
+	 <Art />
+	 <Menu />
+	 <Contact />
+	</main>
+ )
 }
 
 export default App
